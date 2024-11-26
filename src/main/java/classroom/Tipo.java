914750645cd9aa4675a811a5831e3a70dd3face7
Santiagoroseroq <sroseroq@unal.edu.co>
2024-11-26 13:30:42 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,5 +1,3 @@
-package classroom;
-
 public enum Tipo {
     DISCIPLINAR(10), FUNDAMENTACION(20), ELECTIVA(30);
 
@@ -7,17 +5,5 @@ public enum Tipo {
 
     private Tipo(int codigo) {
         this.codigo = codigo;
-    }
-
-    public int getCodigo() {
-        return codigo;
-    }
-
-    @Override
-    public String toString() {
-        return "Tipo{" +
-                "codigo=" + codigo +
-                ", nombre=" + name() +
-                '}';
     }
 }
