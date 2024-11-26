fdc34185972cfa07d5dace76f4892c1cb6ab0fad
Santiagoroseroq <sroseroq@unal.edu.co>
2024-11-26 12:49:50 -0500

    Create AsignaturaTest.java

---
+++ src/test/java/classroom/AsignaturaTest.java
@@ -0,0 +1,25 @@
+package classroom;
+
+import org.junit.jupiter.api.Test;
+
+public class AsignaturaTest {
+    @Test
+    void testCambiarDatos() {
+
+    }
+
+    @Test
+    void testCambiarDatos2() {
+
+    }
+
+    @Test
+    void testCambiarDatos3() {
+
+    }
+
+    @Test
+    void testSetCodigoInterno() {
+
+    }
+}
