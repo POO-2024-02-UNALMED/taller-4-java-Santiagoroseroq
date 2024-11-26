dfe99062886b5f2027143a7e47fb4ade612cb917
Santiagoroseroq <sroseroq@unal.edu.co>
2024-11-26 13:30:57 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -1,40 +1,39 @@
 package classroom;
 
-
 public class Persona {
-    private String nombre;
     private int cedula;
+    private String nombre;
 
     public Persona() {
-        this.nombre = "";
         this.cedula = 0;
+        this.nombre = "";
     }
 
     public Persona(String nombre) {
+        this.cedula = 0;
         this.nombre = nombre;
-        this.cedula = 1;
     }
 
     public Persona(int cedula) {
-        this.nombre = "";
         this.cedula = cedula;
+        this.nombre = "";
     }
 
     public Persona(String nombre, int cedula) {
-        this.nombre = nombre;
         this.cedula = cedula;
+        this.nombre = nombre;
     }
 
     public Persona(int cedula, String nombre) {
-        this.nombre = nombre;
         this.cedula = cedula;
-    }
-
-    public String getNombre() {
-        return nombre;
+        this.nombre = nombre;
     }
 
     public int getCedula() {
         return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
     }
 }
